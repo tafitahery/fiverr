0b7ff752211fce155c9d461281228b0be07707c7
tafitahery <tafitahery@gmail.com>
2024-09-09 16:47:35 +0300

    Trusted By Component Design

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
 import "./home.scss";
 
 export default function Home() {
   return (
     <div className="home">
       <Featured />
+      <TrustedBy />
     </div>
   );
 }
